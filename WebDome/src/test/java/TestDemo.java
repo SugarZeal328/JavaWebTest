91c849fed29d5ce1a687d8ab159ec5f34b593a2b
zellouscheung <897043882@qq.com>
2022-01-28 09:53:55 +0800

    Initial commit

---
+++ WebDome/src/test/java/TestDemo.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class TestDemo {
+    @Test
+    public void test(){
+        //test 表示junit的jar包只能在标记为 Test Sources Root 的文件夹下被调用
+        System.out.println("test is  available");
+    }
+}
